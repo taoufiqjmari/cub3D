7018c93a0328c9d7ba0693bb56afc8751904d007
Taoufiq Jmari <jmaritaoufiq@gmail.com>
2020-12-19 20:54:50 +0100

    Rays casted, need improvement

--- srcs/cub3d.c
+++ srcs/cub3d.c
@@ -6,7 +6,7 @@
 /*   By: tjmari <tjmari@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/25 17:43:56 by tjmari            #+#    #+#             */
-/*   Updated: 2020/12/19 19:40:43 by tjmari           ###   ########.fr       */
+/*   Updated: 2020/12/19 20:52:02 by tjmari           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,7 +69,7 @@ void	render_map(void)
 	}
 }
 
-_Bool	map_has_wall_at(float new_ply_x, float new_ply_y)
+_Bool	map_has_wall_at(float new_x, float new_y)
 {
 	int	map_index_x;
 	int	map_index_y;
@@ -80,8 +80,8 @@ _Bool	map_has_wall_at(float new_ply_x, float new_ply_y)
 	angle = 0;
 	while (angle < 360)
 	{
-		x = new_ply_x + cos(rad(angle));
-		y = new_ply_y + sin(rad(angle));
+		x = new_x + cos(rad(angle));
+		y = new_y + sin(rad(angle));
 		map_index_x = x / TILE_SIZE;
 		map_index_y = y / TILE_SIZE;
 		if (g_map[map_index_y][map_index_x] == ' ' ||
@@ -97,185 +97,142 @@ _Bool	map_has_wall_at(float new_ply_x, float new_ply_y)
 //						RAYS_CODE_START						////
 ////////////////////////////////////////////////////////////////
 
-// _Bool	map_has_wall_at2(float new_ply_x, float new_ply_y)
-// {
-// 	if (new_ply_x < 0 || new_ply_x > IMG_WIDTH || new_ply_y < 0 || new_ply_y > IMG_HEIGHT) {
-// 		return TRUE;
-// 	}
-// 	int mapGridIndexX = floor(new_ply_x / TILE_SIZE);
-// 	int mapGridIndexY = floor(new_ply_y / TILE_SIZE);
-// 	return g_map[mapGridIndexY][mapGridIndexX] != 0;
-// }
+void	cast_ray(float ray_ang, int strip_id)
+{
+	ray_ang = normalize_ang(ray_ang);
 
-// float	distance_between_points(float x1, float y1, float x2, float y2)
-// {
-// 	return (sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)));
-// }
+	int		is_ray_facing_down = ray_ang > rad(0) && ray_ang < rad(180);
+	int		is_ray_facing_up = !is_ray_facing_down;
+	int		is_ray_facing_right = ray_ang > rad(270) || ray_ang < rad(90);
+	int		is_ray_facing_left = !is_ray_facing_right;
 
-// void	cast_ray(float ray_ang, int strip_id)
-// {
-// 	ray_ang = normalize_ang(ray_ang);
+	float	xintercept, yintercept;
+	float	xstep, ystep;
 
-// 	int		is_ray_facing_down = ray_ang > rad(0) && ray_ang < rad(180);
-// 	int		is_ray_facing_up = !is_ray_facing_down;
-// 	int		is_ray_facing_right = ray_ang > rad(270) && ray_ang < rad(90);
-// 	int		is_ray_facing_left = !is_ray_facing_right;
+	/*
+	**	HORIZONTAL RAY_GRID INTERSECTION
+	*/
+	int		found_horz_wall_hit = FALSE;
+	float	horz_wall_hit_x = 0;
+	float	horz_wall_hit_y = 0;
+	int		horz_wall_content = 0;
 
-// 	float	xintercept, yintercept;
-// 	float	xstep, ystep;
+	// Find the y-coordinate of the closest horizontal grid intersection
+	yintercept = floor(g_ply.ply_y / TILE_SIZE) * TILE_SIZE;
+	yintercept += is_ray_facing_down ? TILE_SIZE : 0;
 
-// 	/*
-// 	**	HORIZONTAL RAY_GRID INTERSECTION
-// 	*/
-// 	int		found_horz_wall_hit = FALSE;
-// 	float	horz_wall_hit_x = 0;
-// 	float	horz_wall_hit_y = 0;
-// 	int		horz_wall_content = 0;
+	// Find the x-coordinate of the closest horizontal grid intersection
+	xintercept = g_ply.ply_x + (yintercept - g_ply.ply_y) / tan(ray_ang);
 
-// 	// Find the y-coordinate of the closest horizontal grid intersection
-// 	yintercept = floor(g_ply.ply_y / TILE_SIZE) * TILE_SIZE;
-// 	yintercept += is_ray_facing_down ? TILE_SIZE : 0;
+	// Calculate the increment xstep and ystep
+	ystep = TILE_SIZE;
+	ystep *= is_ray_facing_up ? -1 : 1;
 
-// 	// Find the x-coordinate of the closest horizontal grid intersection
-// 	xintercept = g_ply.ply_x + (yintercept - g_ply.ply_y) / tan(ray_ang);
+	xstep = TILE_SIZE / tan(ray_ang);
+	xstep *= (is_ray_facing_left && xstep > 0) ? -1 : 1;
+	xstep *= (is_ray_facing_right && xstep < 0) ? -1 : 1;
 
-// 	// Calculate the increment xstep and ystep
-// 	ystep = TILE_SIZE;
-// 	ystep *= is_ray_facing_up ? -1 : 1;
+	float next_horz_touch_x = xintercept;
+	float next_horz_touch_y = yintercept;
 
-// 	xstep = TILE_SIZE / tan(ray_ang);
-// 	xstep *= (is_ray_facing_left && xstep > 0) ? -1 : 1;
-// 	xstep *= (is_ray_facing_right && xstep < 0) ? -1 : 1;
+	// Increment xstep and ystep until we find a wall
+	while (next_horz_touch_x >= 0 && next_horz_touch_x <= IMG_WIDTH && next_horz_touch_y >= 0 && next_horz_touch_y <= IMG_HEIGHT)
+	{
+		float x_to_check = next_horz_touch_x;
+		float y_to_check = next_horz_touch_y + (is_ray_facing_up ? -1 : 0);
 
-// 	float next_horz_touch_x = xintercept;
-// 	float next_horz_touch_y = yintercept;
+		if(map_has_wall_at(x_to_check, y_to_check))
+		{
+			horz_wall_hit_x = next_horz_touch_x;
+			horz_wall_hit_y = next_horz_touch_y;
+			horz_wall_content = g_map[(int)floor(y_to_check / TILE_SIZE)][(int)floor(x_to_check / TILE_SIZE)];
+			found_horz_wall_hit = TRUE;
+			break;
+		}
+		else
+		{
+			next_horz_touch_x += xstep;
+			next_horz_touch_y += ystep;
+		}
+	}
 
-// 	// Increment xstep and ystep until we find a wall
-// 	while (next_horz_touch_x >= 0 && next_horz_touch_x <= IMG_WIDTH && next_horz_touch_y >= 0 && next_horz_touch_y <= IMG_HEIGHT)
-// 	{
-// 		float x_to_check = next_horz_touch_x;
-// 		float y_to_check = next_horz_touch_y + (is_ray_facing_up ? -1 : 0);
+	/*
+	**	VERTICAL RAY_GRID INTERSECTION
+	*/
+	int		found_vert_wall_hit = FALSE;
+	float	vert_wall_hit_x = 0;
+	float	vert_wall_hit_y = 0;
+	int		vert_wall_content = 0;
 
-// 		if(map_has_wall_at2(x_to_check, y_to_check))
-// 		{
-// 			horz_wall_hit_x = next_horz_touch_x;
-// 			horz_wall_hit_y = next_horz_touch_y;
-// 			horz_wall_content = g_map[(int)floor(y_to_check / TILE_SIZE)][(int)floor(x_to_check / TILE_SIZE)];
-// 			found_horz_wall_hit = TRUE;
-// 			break;
-// 		}
-// 		else
-// 		{
-// 			next_horz_touch_x += xstep;
-// 			next_horz_touch_y += ystep;
-// 		}
-// 	}
+	// Find the x-coordinate of the closest horizontal grid intersection
+	xintercept = floor(g_ply.ply_x / TILE_SIZE) * TILE_SIZE;
+	xintercept += is_ray_facing_right ? TILE_SIZE : 0;
 
-// 	/*
-// 	**	VERTICAL RAY_GRID INTERSECTION
-// 	*/
-// 	int		found_vert_wall_hit = FALSE;
-// 	float	vert_wall_hit_x = 0;
-// 	float	vert_wall_hit_y = 0;
-// 	int		vert_wall_content = 0;
+	// Find the x-coordinate of the closest horizontal grid intersection
+	yintercept = g_ply.ply_y + (xintercept - g_ply.ply_x) * tan(ray_ang);
 
-// 	// Find the x-coordinate of the closest horizontal grid intersection
-// 	xintercept = floor(g_ply.ply_x / TILE_SIZE) * TILE_SIZE;
-// 	xintercept += is_ray_facing_right ? TILE_SIZE : 0;
+	// Calculate the increment xstep and ystep
+	xstep = TILE_SIZE;
+	xstep *= is_ray_facing_left ? -1 : 1;
 
-// 	// Find the x-coordinate of the closest horizontal grid intersection
-// 	yintercept = g_ply.ply_y + (xintercept - g_ply.ply_x) * tan(ray_ang);
+	ystep = TILE_SIZE * tan(ray_ang);
+	ystep *= (is_ray_facing_up && ystep > 0) ? -1 : 1;
+	ystep *= (is_ray_facing_down && ystep < 0) ? -1 : 1;
 
-// 	// Calculate the increment xstep and ystep
-// 	xstep = TILE_SIZE;
-// 	xstep *= is_ray_facing_left ? -1 : 1;
+	float next_vert_touch_x = xintercept;
+	float next_vert_touch_y = yintercept;
 
-// 	ystep = TILE_SIZE * tan(ray_ang);
-// 	ystep *= (is_ray_facing_up && ystep > 0) ? -1 : 1;
-// 	ystep *= (is_ray_facing_down && ystep < 0) ? -1 : 1;
+	// Increment xstep and ystep until we find a wall
+	while (next_vert_touch_x >= 0 && next_vert_touch_x <= IMG_WIDTH && next_vert_touch_y >= 0 && next_vert_touch_y <= IMG_HEIGHT)
+	{
+		float x_to_check = next_vert_touch_x + (is_ray_facing_left ? -1 : 0);
+		float y_to_check = next_vert_touch_y;
 
-// 	float next_vert_touch_x = xintercept;
-// 	float next_vert_touch_y = yintercept;
+		if(map_has_wall_at(x_to_check, y_to_check))
+		{
+			vert_wall_hit_x = next_vert_touch_x;
+			vert_wall_hit_y = next_vert_touch_y;
+			vert_wall_content = g_map[(int)floor(y_to_check / TILE_SIZE)][(int)floor(x_to_check / TILE_SIZE)];
+			found_vert_wall_hit = TRUE;
+			break;
+		}
+		else
+		{
+			next_vert_touch_x += xstep;
+			next_vert_touch_y += ystep;
+		}
+	}
 
-// 	// Increment xstep and ystep until we find a wall
-// 	while (next_vert_touch_x >= 0 && next_vert_touch_x <= IMG_WIDTH && next_vert_touch_y >= 0 && next_vert_touch_y <= IMG_HEIGHT)
-// 	{
-// 		float x_to_check = next_vert_touch_x + (is_ray_facing_left ? -1 : 0);
-// 		float y_to_check = next_vert_touch_y;
+	// Calculate both horizontal and vertical hit distances and choose the smallest one
+	float	horz_hit_distance = found_horz_wall_hit
+		? distance_between_points(g_ply.ply_x, g_ply.ply_y, horz_wall_hit_x, horz_wall_hit_y)
+		: INT_MAX;
+	float	vert_hit_distance = found_vert_wall_hit
+		? distance_between_points(g_ply.ply_x, g_ply.ply_y, vert_wall_hit_x, vert_wall_hit_y)
+		: INT_MAX;
 
-// 		if(map_has_wall_at2(x_to_check, y_to_check))
-// 		{
-// 			vert_wall_hit_x = next_vert_touch_x;
-// 			vert_wall_hit_y = next_vert_touch_y;
-// 			vert_wall_content = g_map[(int)floor(y_to_check / TILE_SIZE)][(int)floor(x_to_check / TILE_SIZE)];
-// 			found_vert_wall_hit = TRUE;
-// 			break;
-// 		}
-// 		else
-// 		{
-// 			next_vert_touch_x += xstep;
-// 			next_vert_touch_y += ystep;
-// 		}
-// 	}
-
-// 	// Calculate both horizontal and vertical hit distances and choose the smallest one
-// 	float	horz_hit_distance = found_horz_wall_hit
-// 		? distance_between_points(g_ply.ply_x, g_ply.ply_y, horz_wall_hit_x, horz_wall_hit_y)
-// 		: INT_MAX;
-// 	float	vert_hit_distance = found_vert_wall_hit
-// 		? distance_between_points(g_ply.ply_x, g_ply.ply_y, vert_wall_hit_x, vert_wall_hit_y)
-// 		: INT_MAX;
-
-// 	if(vert_hit_distance < horz_hit_distance)
-// 	{
-// 		g_rays[strip_id].distance = vert_hit_distance;
-// 		g_rays[strip_id].wall_hit_x = vert_wall_hit_x;
-// 		g_rays[strip_id].wall_hit_y = vert_wall_hit_y;
-// 		g_rays[strip_id].wall_hit_content = vert_wall_content;
-// 		g_rays[strip_id].was_hit_ver = TRUE;
-// 	}
-// 	else
-// 	{
-// 		g_rays[strip_id].distance = horz_hit_distance;
-// 		g_rays[strip_id].wall_hit_x = horz_wall_hit_x;
-// 		g_rays[strip_id].wall_hit_y = horz_wall_hit_y;
-// 		g_rays[strip_id].wall_hit_content = horz_wall_content;
-// 		g_rays[strip_id].was_hit_ver = FALSE;
-// 	}
-// 	g_rays[strip_id].ray_ang = ray_ang;
-// 	g_rays[strip_id].is_ray_down = is_ray_facing_down;
-// 	g_rays[strip_id].is_ray_up = is_ray_facing_up;
-// 	g_rays[strip_id].is_ray_left = is_ray_facing_left;
-// 	g_rays[strip_id].is_ray_right = is_ray_facing_right;
-// }
-
-// void	update_rays(void)
-// {
-// 	float	ray_ang;
-// 	int		strip_id;
-
-// 	ray_ang = g_ply.rotation_ang - (FOV_ANG / 2);
-// 	strip_id = 0;
-// 	while (strip_id < NUM_RAYS)
-// 	{
-// 		cast_ray(ray_ang, strip_id);
-// 		ray_ang += FOV_ANG / NUM_RAYS;
-// 		strip_id++;
-// 	}
-// }
-
-// void	render_rays(void)
-// {
-// 	int	i;
-
-// 	i = 0;
-// 	while (i < NUM_RAYS)
-// 	{
-// 		line(g_ply.ply_x, g_ply.ply_y,
-// 				g_rays[i].wall_hit_x, g_rays[i].wall_hit_x);
-// 		i++;
-// 	}
-// }
+	if(vert_hit_distance < horz_hit_distance)
+	{
+		g_rays[strip_id].distance = vert_hit_distance;
+		g_rays[strip_id].wall_hit_x = vert_wall_hit_x;
+		g_rays[strip_id].wall_hit_y = vert_wall_hit_y;
+		g_rays[strip_id].wall_hit_content = vert_wall_content;
+		g_rays[strip_id].was_hit_ver = TRUE;
+	}
+	else
+	{
+		g_rays[strip_id].distance = horz_hit_distance;
+		g_rays[strip_id].wall_hit_x = horz_wall_hit_x;
+		g_rays[strip_id].wall_hit_y = horz_wall_hit_y;
+		g_rays[strip_id].wall_hit_content = horz_wall_content;
+		g_rays[strip_id].was_hit_ver = FALSE;
+	}
+	g_rays[strip_id].ray_ang = ray_ang;
+	g_rays[strip_id].is_ray_down = is_ray_facing_down;
+	g_rays[strip_id].is_ray_up = is_ray_facing_up;
+	g_rays[strip_id].is_ray_left = is_ray_facing_left;
+	g_rays[strip_id].is_ray_right = is_ray_facing_right;
+}
 
 ////////////////////////////////////////////////////////////////
 //						RAYS_CODE_END						////
@@ -334,7 +291,7 @@ void	setup(void)
 void	update(void)
 {
 	update_player();
-	// update_rays();
+	update_rays();
 }
 
 void	render(void)
@@ -342,7 +299,7 @@ void	render(void)
 	update();
 	render_map();
 	render_player();
-	// render_rays();
+	render_rays();
 	mlx_put_image_to_window(g_mlx.mlx, g_mlx.mlx_win,
 								g_img.img, g_img.img_x, g_img.img_y);
 }
@@ -350,6 +307,7 @@ void	render(void)
 int		main(void)
 {
 	setup();
+	render_map();
 	render();
 	mlx_hook(g_mlx.mlx_win, 2, 1L << 0, key_pressed, &g_ply);
 	mlx_hook(g_mlx.mlx_win, 17, 0L, red_cross, &g_mlx);
