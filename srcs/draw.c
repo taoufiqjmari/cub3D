af05e86539876f54e6d53ccc0f43bb066e1691c0
Taoufiq Jmari <jmaritaoufiq@gmail.com>
2021-01-16 12:20:40 +0100

    accepting minimum resolution

--- srcs/draw.c
+++ srcs/draw.c
@@ -6,7 +6,7 @@
 /*   By: tjmari <tjmari@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/28 17:01:08 by tjmari            #+#    #+#             */
-/*   Updated: 2021/01/13 17:44:54 by tjmari           ###   ########.fr       */
+/*   Updated: 2021/01/16 11:19:41 by tjmari           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,13 @@
 void	mlx_pixel_put_img(int x, int y, int color)
 {
 	char	*dst;
-
+	if (x >= WIN_WIDTH || y  >= WIN_HEIGHT)
+		return ;
 	dst = g_mlx.addr + (y * g_mlx.line_length +
 							x * (g_mlx.bits_per_pixel / 8));
 	*(unsigned int*)dst = color;
+	// g_mlx.addr[y * g_mlx.line_length +
+	// 						x] = color;
 }
 
 void	rect(int i, int j, int color)
